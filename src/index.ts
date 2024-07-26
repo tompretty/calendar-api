021d1888b7292a3b015cfd436be3b9a0365a9129
TomPretty <tompretty2@gmail.com>
2024-07-26 14:51:30 +0100

    basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,59 @@
-export * from "./greet.js";
-export * from "./types.js";
+import express from "express";
+import bodyParser from "body-parser";
+import { z } from "zod";
+
+const app = express();
+const port = 3000;
+
+interface CalendarEvent {
+	name: string;
+	startAt: Date;
+	endAt: Date;
+}
+
+const isoDateSchema = z.string().datetime().pipe(z.coerce.date());
+
+const apiCalendarEventSchema = z.object({
+	name: z.string(),
+	startAt: isoDateSchema,
+	endAt: isoDateSchema,
+});
+
+const EVENTS: CalendarEvent[] = [
+	{
+		name: "Driving lesson",
+		startAt: new Date(2024, 0, 1, 9, 0, 0),
+		endAt: new Date(2024, 0, 1, 12, 0, 0),
+	},
+	{
+		name: "Pick up shopping",
+		startAt: new Date(2024, 0, 1, 17, 0, 0),
+		endAt: new Date(2024, 0, 1, 18, 0, 0),
+	},
+];
+
+app.get("/", (_req, res) => {
+	res.send("Hello World!");
+});
+
+app.get("/events", (_req, res) => {
+	res.json({ events: EVENTS });
+});
+
+app.post("/events", bodyParser.json(), (req, res) => {
+	console.log(req.body);
+	const result = apiCalendarEventSchema.safeParse(req.body);
+
+	if (!result.success) {
+		res.sendStatus(400);
+		return;
+	}
+
+	EVENTS.push(result.data);
+
+	res.sendStatus(203);
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
